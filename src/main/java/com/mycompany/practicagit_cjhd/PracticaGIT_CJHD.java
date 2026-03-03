af0c1f4134a2d2d1d86d1305640dc3b091d2a30c
Pomuch19 <Pomuch19@DESKTOP-4JGGI9F>
2026-03-03 07:36:19 -0600

    feat: estructura inicial del proyecto

---
+++ src/main/java/com/mycompany/practicagit_cjhd/PracticaGIT_CJHD.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practicagit_cjhd;
+
+/**
+ *
+ * @author Pomuch19
+ */
+public class PracticaGIT_CJHD {
+
+    public static void main(String[] args) {
+        System.out.println("--- Sistema de gestion de alumnos ---");
+        
+    }
+}
