95df816a00e8feff913b90181f0f747c37d3f9a9
Pomuch19 <Pomuch19@DESKTOP-4JGGI9F>
2026-03-09 07:35:28 -0600

    fix: añadida calculadora basica

--- src/main/java/com/mycompany/practicagit_cjhd/PracticaGIT_CJHD.java
+++ src/main/java/com/mycompany/practicagit_cjhd/PracticaGIT_CJHD.java
@@ -15,7 +15,22 @@ public class PracticaGIT_CJHD {
     public static void main(String[] args) {
         System.out.println("--- Sistema de gestion de alumnos ---");
         System.out.println("Bienvenido, usuario de estudiante.");
+       {   Scanner cin = new Scanner(System.in);// declaracion para la variable para la lectura de los datos de entrada 
      
+     // declarar variable 
+     int n1, n2, suma;
+     
+     System.out.print("introduce primer numero: "); 
+     n1 = cin.nextInt(); // el valor que intruduce el usuario es asignado a la variable n1
+     
+     System.out.print("intruduce segundo numero: ");
+     n2 = cin.nextInt(); // el que intruduce el usario es asignado a la variable n2
+     
+     //poseso
+     suma = n1 + n2;
+     
+     // salida 
+     System.out.println("elresultado es " + suma);}
     }// esta llve indica el fin del programa 
     
 }//Esta llave indica el cierre de la clase 
