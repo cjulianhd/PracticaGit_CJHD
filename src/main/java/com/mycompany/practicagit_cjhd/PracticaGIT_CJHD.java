bf8d1200b128f850d99bfcb7fa58e3bffc0deaba
Pomuch19 <Pomuch19@DESKTOP-4JGGI9F>
2026-03-09 07:33:58 -0600

    fix: añadida calculadora basica

--- src/main/java/com/mycompany/practicagit_cjhd/PracticaGIT_CJHD.java
+++ src/main/java/com/mycompany/practicagit_cjhd/PracticaGIT_CJHD.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.practicagit_cjhd;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Pomuch19
@@ -13,6 +15,8 @@ public class PracticaGIT_CJHD {
     public static void main(String[] args) {
         System.out.println("--- Sistema de gestion de alumnos ---");
         System.out.println("Bienvenido, usuario de estudiante.");
-        
-    }
-}
+     
+    }// esta llve indica el fin del programa 
+    
+}//Esta llave indica el cierre de la clase 
+   
